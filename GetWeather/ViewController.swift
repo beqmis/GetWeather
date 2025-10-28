7f4134bee7b34197b1898bfc07f6035deecbc616
yashaLGBTBristleback <pochtaschule@gmail.com>
2025-10-28 21:30:27 +0300

    Initial Commit

---
+++ GetWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetWeather
+//
+//  Created by Яков Демиденко on 10/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GetWeatherTests/GetWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  GetWeatherTests.swift
+//  GetWeatherTests
+//
+//  Created by Яков Демиденко on 10/28/25.
+//
+
+import Testing
+@testable import GetWeather
+
+struct GetWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
