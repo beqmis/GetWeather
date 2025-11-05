0be08dddee7fd0e99544c6566279f4f85f84db6e
yashaLGBTBristleback <pochtaschule@gmail.com>
2025-11-06 00:13:15 +0300

    Workable textField and sink in progress

--- GetWeather/MVVM/SearchWetherViewController.swift
+++ GetWeather/MVVM/SearchWetherViewController.swift
@@ -8,47 +8,77 @@
 import Foundation
 import UIKit
 import SnapKit
+import Combine
+
+extension UITextField{
+    var textPublisher:AnyPublisher<String,Never>{
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap { $0.object as? UITextField }
+            .map { $0.text ?? "" }
+            .eraseToAnyPublisher()
+    }
+}
 
 
-
-class SearchWetherViewController: UIViewController, UISearchBarDelegate
+class SearchWetherViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource
 {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.locations.count
+    }
     
-    let searchBar:UISearchBar = UISearchBar()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        let location = viewModel.locations[indexPath.row]
+        cell.textLabel?.text = location.currentAddress
+        
+        return cell
+    }
+    
+    
     let searchTextField:UITextField = UITextField()
+    let outputsTableView:UITableView = UITableView()
     
-    let outputs:UITableView = UITableView()
+    private var cancellables = Set<AnyCancellable>()
+    
+    var viewModel = WeatherViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         
-//        self.searchBar.placeholder = "Search"
-//        self.searchBar.sizeToFit()
-//        self.searchBar.backgroundColor = .blue
-//        self.searchBar.delegate = self
-        
         searchTextField.text? = "Moscow"
         searchTextField.backgroundColor = .green
         
-        outputs.backgroundColor = .red
+        outputsTableView.backgroundColor = .red
         
         
         setupUI()
+        
+        searchTextField.textPublisher
+            .removeDuplicates()
+            .sink { [weak self] text in
+                // Handle text changes here, e.g., forward to view model
+                // self?.viewModel.search(query: text)
+                print("Search text:", text)
+            }
+            .store(in: &cancellables)
+        
+        viewModel.$locations
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] locations in
+                print("Locations:", locations)
+                self?.outputsTableView.reloadData()
+            }
+            .store(in: &cancellables)
     }
     
     
     
     func setupUI()
     {
-//        view.addSubview(searchBar)
-//        searchBar.snp.makeConstraints { make in
-//            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(8)
-//            make.leading.equalToSuperview().offset(16)
-//            make.trailing.equalToSuperview().inset(16)
-//            make.height.equalTo(44)
-//        }
+
         
         view.addSubview(searchTextField)
         searchTextField.snp.makeConstraints { make in
@@ -58,8 +88,8 @@ class SearchWetherViewController: UIViewController, UISearchBarDelegate
             make.height.equalTo(44)
         }
         
-        view.addSubview(outputs)
-        outputs.snp.makeConstraints { make in
+        view.addSubview(outputsTableView)
+        outputsTableView.snp.makeConstraints { make in
             make.top.equalTo(searchTextField.snp.bottom).offset(10)
             make.left.right.bottom.equalToSuperview()
         }
--- GetWeather/MVVM/WeatherViewMode.swift
+++ GetWeather/MVVM/WeatherViewMode.swift
@@ -6,10 +6,42 @@
 //
 
 import Foundation
-
+import Combine
 
 
 class WeatherViewModel
 {
     
+    @Published var text:String = ""
+    @Published var locations: [AddresLocation] = []
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    private let locationService: LocationServiceProtocol
+    
+    init(locationService: LocationServiceProtocol = YandexGeoService())
+    {
+        self.locationService = locationService
+        setupBindings()
+        //locationService.fetchLocations(query: text)
+    }
+    
+    private func setupBindings()
+    {
+        $text
+            .filter { $0.count >= 3 }
+            .debounce(for: .seconds(0.6), scheduler: DispatchQueue.main)
+            .removeDuplicates()
+            .flatMap { [unowned self] query -> AnyPublisher<[AddresLocation], Never> in
+                self.locationService
+                    .fetchLocations(query: query)
+                    .replaceError(with: [])
+                    .eraseToAnyPublisher()
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] locations in
+                self?.locations = locations
+            }
+            .store(in: &cancellables)
+    }
 }
